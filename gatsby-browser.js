ae6f935c26af98a0c4377057242107770ab513c4
Mohammed Y. Alnajdi <mohdnagfy@gmail.com>
2022-05-28 02:17:57 +0300

    warrning for MDXProvide can be ignored

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { ThemeProvider } from './src/context/ThemeContext';
 
+import { MDXProvider } from '@mdx-js/react';
+import { Author } from './src/shortcodes';
+
+const shortcodes = { Author };
 
 export const wrapRootElement = ({ element }) => (
+  <MDXProvider components={shortcodes}>
     <ThemeProvider>{element}</ThemeProvider>
+  </MDXProvider>
 );
--- src/layout/index.js
+++ src/layout/index.js
@@ -6,10 +6,7 @@ import Footer from '../components/Footer';
 import config from '../../data/SiteConfig';
 import favicon from '../images/sharektech.png';
 import '../styles/main.scss';
-import { MDXProvider } from '@mdx-js/react';
-import { Author } from '../shortcodes';
 
-const shortcodes = { Author };
 
 export default class MainLayout extends Component {
   static contextType = ThemeContext;
@@ -26,7 +23,7 @@ export default class MainLayout extends Component {
     }
 
     return (
-      <MDXProvider components={shortcodes}>
+      <>
         <Helmet
           bodyAttributes={{
             class: `theme ${themeClass}`
@@ -38,7 +35,7 @@ export default class MainLayout extends Component {
         <Navigation menuLinks={config.menuLinks} />
         <main id="main-content">{children}</main>
         <Footer />
-      </MDXProvider>
+      </>
     );
   }
 }
